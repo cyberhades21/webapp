153e93f9696990cf4b2b48ff96ce84523ec9a9b3
cyberhades21 <79735414+cyberhades21@users.noreply.github.com>
2022-09-26 20:25:30 +0530

    Templates and Splitted Main - PHOTGRAM

---
+++ _libs/load.php
@@ -0,0 +1,5 @@
+<?php
+function load_template($name){
+    include "__template/$name.php";
+}
+?>
--- index.php
+++ index.php
@@ -1,3 +1,63 @@
+<?php
+include '_libs/load.php'
+?>
 
 
 
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>PHOTOGRAM</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/album/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="/app/assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+  </head>
+  <body>
+    
+<header>
+  <?php load_template('header'); ?>
+</header>
+
+<main>
+
+  <?php load_template('landing'); ?>
+
+  <?php load_template('_photogram');?>
+
+</main>
+
+<?php load_template('_footer');?>
+
+
+    <script src="/app/assets/dist/js/bootstrap.bundle.min.js"></script>
+
+      
+  </body>
+</html>
