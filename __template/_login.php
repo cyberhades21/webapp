0a48f48caa61b36cb0994b693a592ac987f599ab
cyberhades21 <79735414+cyberhades21@users.noreply.github.com>
2022-10-03 20:47:46 +0530

    Login -Hover CSS

--- __template/_login.php
+++ __template/_login.php
@@ -1,14 +1,14 @@
 <main class="form-signin">
-  <form>
-    <img class="mb-4" src="assets/logo.svg" alt="" width="72" height="57">
+  <form method="post" action="test.php">
+    <img class="mb-4" src="assets/logo.svg" alt="" width="72" height="400">
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
     <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <input name="email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
       <label for="floatingInput">Email address</label>
     </div>
     <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
       <label for="floatingPassword">Password</label>
     </div>
 
@@ -17,7 +17,12 @@
         <input type="checkbox" value="remember-me"> Remember me
       </label>
     </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-muted">© 2017–2021</p>
+    <button class="w-100 btn btn-lg btn-primary hvr-grow" type="submit">Sign in</button>
+    <p class="mt-5 mb-3 text-muted"><t>© 2017 – 2021 </p>
   </form>
 </main>
+<?php
+echo $user;
+echo $password;
+?>
+        
